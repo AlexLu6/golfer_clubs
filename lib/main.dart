5f0dcc72a6274104527ee5efff62a497e102c8fc
FlutLab.io <>
2022-05-15 04:36:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -589,7 +589,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                   if (!snapshot3.hasData)
                                     return const CircularProgressIndicator();
                                   else
-                                    return Image.network(snapshot3.data!.toString());
+                                    return Image.network(snapshot3.data!.toString(), fit: BoxFit.fitWidth);
                                 }),
                             trailing: Icon(Icons.keyboard_arrow_right),
                             onTap: () async {
