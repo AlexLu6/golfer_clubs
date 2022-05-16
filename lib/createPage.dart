af6b63ec465ec8a04d055efdd12d6b9ed1d87872
FlutLab.io <>
2022-05-16 02:23:55 +0000

    Autosave

--- lib/createPage.dart
+++ lib/createPage.dart
@@ -963,8 +963,8 @@ class ShowActivityPage extends MaterialPageRoute<int> {
                           columns: [
                             {'title': Language.of(context).rank, 'index': 1, 'key': 'rank', 'editable': false},
                             {'title': Language.of(context).total, 'index': 2, 'key': 'total', 'editable': false},
-                            {'title': Language.of(context).name, 'index': 3, 'key': 'name', 'editable': false, 'widthFactor': 0.2},
-                            {'title': Language.of(context).net, 'index': 4, 'key': 'net', 'editable': false, 'widthFactor': 0.16},
+                            {'title': Language.of(context).name, 'index': 3, 'key': 'name', 'editable': false, 'widthFactor': 0.18},
+                            {'title': Language.of(context).net, 'index': 4, 'key': 'net', 'editable': false, 'widthFactor': 0.15},
                             {'title': 'E', 'index': 5, 'key': 'EG', 'editable': false, 'widthFactor': 0.1},
                             {'title': 'B', 'index': 6, 'key': 'BD', 'editable': false, 'widthFactor': 0.1},
                             {'title': 'P', 'index': 7, 'key': 'PAR', 'editable': false, 'widthFactor': 0.1},
