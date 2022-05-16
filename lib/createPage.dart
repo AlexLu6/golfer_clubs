fa5749c2b8cfdc1152111f25632c13114c0cf34f
FlutLab.io <>
2022-05-16 02:35:08 +0000

    Autosave

--- lib/createPage.dart
+++ lib/createPage.dart
@@ -979,21 +979,23 @@ class ShowActivityPage extends MaterialPageRoute<int> {
                           child: Text(teeOffPass && alreadyIn ? Language.of(context).enterScore
                                                   : alreadyIn ? Language.of(context).cancel : Language.of(context).apply),
                           onPressed: () async {
+                            bool stay = false;
                             if (teeOffPass && alreadyIn) {
                               if ((course["zones"]).length > 2) {
                                 List zones = await selectZones(context, course);
                                 if (zones.isNotEmpty)
                                   Navigator.push(context, newScorePage(course, uName, zone0: zones[0], zone1: zones[1])).then((value) {
-                                    if (value ?? false) {updateScore(); setState(() {});}
+                                    if (value ?? false) updateScore();
+                                    else stay = true;
                                   });
                               } else {
                                 Navigator.push(context, newScorePage(course, uName)).then((value) {
-                                  if (value ?? false) {updateScore(); setState(() {});}
+                                  if (value ?? false) updateScore();
+                                  else stay = true;
                                 });
                               }
-                            } else {
-                              Navigator.of(context).pop(teeOffPass ? 0 : alreadyIn ? -1 : 1);
                             }
+                            if (!stay) Navigator.of(context).pop(teeOffPass ? 0 : alreadyIn ? -1 : 1);
                           }),
                   const SizedBox(height: 16.0),
                   Text(Language.of(context).actRemarks + activity.data()!['remarks']),
