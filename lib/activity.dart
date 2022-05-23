1022c775cf32e6dc6aff02c43a3888c0d06b1af7
FlutLab.io <>
2022-05-23 07:11:12 +0000

    Autosave

--- lib/activity.dart
+++ lib/activity.dart
@@ -259,7 +259,7 @@ class ShowActivityPage extends MaterialPageRoute<int> {
                       thAlignment: TextAlign.center,
                       columnRatio: 0.2,
                       columns: [
-                        {"title": Language.of(context).tableGroup, 'index': 1, 'key': 'row', 'editable': false, 'widthFactor': 0.16},
+                        {"title": Language.of(context).tableGroup, 'index': 1, 'key': 'row', 'editable': false, 'widthFactor': 0.15},
                         {"title": "A", 'index': 2, 'key': 'c1', 'editable': false},
                         {"title": "B", 'index': 3, 'key': 'c2', 'editable': false},
                         {"title": "C", 'index': 4, 'key': 'c3', 'editable': false},
