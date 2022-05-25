ca5dc378fd25fa5cddfff05bf842ade1258eb425
FlutLab.io <>
2022-05-25 14:04:33 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -220,12 +220,12 @@ class _MyHomePageState extends State<MyHomePage> {
         if (!snapshot.hasData)
           return const CircularProgressIndicator();
         else
-          return Flexible(child: InteractiveViewer(
+          return Column(children: [Expanded(flex: 2, child: InteractiveViewer(
             //panEnabled: false,
             minScale: 0.8,
             maxScale: 2.5,
             child: Image.network(snapshot.data!.toString())
-          ));
+          ))]);
       }
     );
   }
