64f479b7a1246596ce39fbac73b525f037edceff
FlutLab.io <>
2022-06-01 04:44:33 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
         if (expiredDate == '') {
           expiredDate = items['expired'].toDate().toString();
           prefs!.setString('expired', expiredDate);
+          print(expiredDate);
         }
         isExpired = items['expired'].compareTo(Timestamp.now()) < 0;
         setState(() => isRegistered = true);
@@ -110,6 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
       Language.of(context).usage,  // "Program Usage"
       Language.of(context).purchase
     ];
+    if (expiredDate.length > 0)
     isExpired = Timestamp.fromDate(DateTime.parse(expiredDate.substring(0, 16))).compareTo(Timestamp.now()) < 0;
     return Scaffold(
       appBar: AppBar(
